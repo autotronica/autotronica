1cb12ddd6d4473b137fae026eb9c1ddd618b85ab
Hector <hdseba.uch@gmail.com>
2017-08-22 16:25:15 -0300

    modificacion de html y arreglo de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,9 @@ import { RouterModule, Routes } from '@angular/router';
 import {VehiculoComponent} from './components/vehiculo/vehiculo.component';
 import {HistorialComponent} from './components/vehiculo/historial.component';
 const APP_ROUTES: Routes = [
-{ path: 'vehiculo/historial', component: HistorialComponent },
-  { path: 'vehiculo/detalle/:id', component: VehiculoComponent },
-  { path: '**', pathMatch: 'full', redirectTo: 'vehiculo/historial' }
+  { path: 'vehiculo', component: VehiculoComponent },
+{ path: 'vehiculo/historial/:id', component: HistorialComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'vehiculo' }
 ];
 
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
