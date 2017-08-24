a370d4f6923eb3abe98918af5a78c96a905c7b5e
Hector <hdseba.uch@gmail.com>
2017-08-24 17:38:48 -0300

    Se modificaron los archivos del componente vehiculo para las peticiones http de pruebas conectadas a nodejs

--- src/app/components/vehiculo/vehiculo.component.ts
+++ src/app/components/vehiculo/vehiculo.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Router,ActivatedRoute,Params} from '@angular/router';
+import {VehiculoService} from '../../services/vehiculo.service';
+import {Vehiculo} from '../../models/vehiculo.models';
 
 @Component({
   selector: 'app-vehiculo',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vehiculo.component.css']
 })
 export class VehiculoComponent implements OnInit {
+  public vehiculos:Vehiculo[];
+  public errormessage:any;
+  public loading:boolean;
 
-  constructor() { }
+  constructor(private router:Router,private route:ActivatedRoute,private vehiculoservice:VehiculoService) {
+    this.consultaVehiculos();
+  }
 
   ngOnInit() {
   }
 
+  consultaVehiculos(){
+    this.loading=true;
+    this.vehiculoservice.consultaVehiculos().subscribe(res=>{
+      this.vehiculos=res.vehiculos;
+      if(!this.vehiculos){
+        console.log("No hay vehiculos cargados");
+      }else{
+        this.loading=false;
+      }
+    },error=>{
+      this.errormessage=<any>error;
+      console.log("Error en la consultaVehiculos: "+this.errormessage);
+    });
+  }
 }
