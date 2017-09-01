da690d1341b62f189fc52b81a3352067aaa1aa24
Hector <hdseba.uch@gmail.com>
2017-08-31 22:47:18 -0300

    se agrego la url de la pagina comentada para su utilizacion en el vps

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,4 +1,5 @@
 export var GLOBAL ={
-  url:"http://localhost:8080/autotronica/",
-  ip:"107.0.18"
+  url:"http://localhost:3000/autotronica/",
+   //url:"http://104.193.111.115:3000/autotronica/",
+  ip:"104.193.111.115"
 }
